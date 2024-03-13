7ae37fdc993241f1fa28358784d935b3b690edea
time1043 <1665434994@qq.com>
2024-03-13 10:59:49 +0800

    MODEL:CreateAt:gorm:default:CURRENT_TIMESTAMP(3)

--- gvb-server/models/enter.go
+++ gvb-server/models/enter.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type MODEL struct {
-	ID       uint      `gorm:"primaryKey" json:"id"`                       // 主键ID
-	CreateAt time.Time `json:"create_at" gorm:"default:CURRENT_TIMESTAMP"` // 创建时间
-	UpdateAt time.Time `json:"update_at" gorm:"default:CURRENT_TIMESTAMP"` // 更新时间
+	ID       uint      `gorm:"primaryKey" json:"id"`                          // 主键ID
+	CreateAt time.Time `json:"create_at" gorm:"default:CURRENT_TIMESTAMP(3)"` // 创建时间
+	UpdateAt time.Time `json:"update_at" gorm:"default:CURRENT_TIMESTAMP(3)"` // 更新时间
 }
 
 type PageInfo struct {
